42597c07d2a56c7abb991d9456977971215fa213
Artavazdng256 <40898141+Artavazdng256@users.noreply.github.com>
2021-06-22 20:51:18 +0400

    Update ue4_build.bat
    Added UnrealLightmass build.

--- ue4_build.bat
+++ ue4_build.bat
@@ -1,3 +1,5 @@
 call "%CD%/Setup.bat" --force
 call "%CD%/GenerateProjectFiles.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "%CD%/UE4.sln" -target:"Engine\UE4" -property:Platform=Win64;Configuration="Development Editor" -verbosity:diagnostic
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" "%CD%/UE4.sln" -target:"Programs\UnrealLightmass" -property:Platform=Win64;Configuration="Development Editor" -verbosity:diagnostic
+
